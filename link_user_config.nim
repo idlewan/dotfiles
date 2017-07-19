950a7950a6e3d4120495d26cd54fd6a1a0485de3
Erwan Ameil <wan@idlewan.com>
2017-07-19 15:44:04 +0100

    Better debug info and resistability with symbolic links for script

--- link_user_config.nim
+++ link_user_config.nim
@@ -11,7 +11,7 @@ Options:
   --version     Show version
 """
 
-import docopt, os, times
+import docopt, os, times, colorize, strutils
 
 let args = docopt(doc, version = "Link User Config 0.1")
 
@@ -32,6 +32,13 @@ if not existsDir(dotfiles):
 if not existsDir(dest_config):
   quit "The destination config folder \"" & dest_config & "\" is not a valid folder"
 
+var indent = 0
+proc info*(msgs: varargs[string, `$`]) =
+  write stdout, "  ".repeat(indent)
+  for msg in msgs:
+    write stdout,  msg
+  writeLn stdout, ""
+
 var backup_folder = ""
 proc backup(dest_folder, filename: string) =
   # ensure that backup folder is present before copying to it
@@ -40,7 +47,7 @@ proc backup(dest_folder, filename: string) =
       getLocalTime(getTime()), "yyyy-MM-dd'_'HH'h'mm"
     )
     createDir(backup_folder)
-  echo "backing up ", filename
+  info "backing up ", fgGreen(filename)
   moveFile(dest_folder / filename, backup_folder / filename)
 
 let ignore_list = @[
@@ -61,14 +68,16 @@ let partial_configs = @[
   "vifm"
 ]
 
+
 proc create_links(source, destination: string, skip = ignore_list) =
-  echo "==  create links from ", source, " to ", destination, "  =="
+  info "==  create links from ", source, " to ", fgBlue(destination), "  =="
+  indent += 1
   for f in walkDir(source, relative=true):
     let dest_file = destination / f.path
     var source_file = source / f.path
 
     if f.path in skip:
-      echo "    (skipping ", f.path, ")"
+      info fgDarkGray("(skipping "), f.path, fgDarkGray(")")
       continue
 
     if f.path in partial_configs:
@@ -81,11 +90,18 @@ proc create_links(source, destination: string, skip = ignore_list) =
     if symlinkExists(dest_file):
       var cur_symlink_path = dest_file
       while symlinkExists(cur_symlink_path):
+        info fgDarkGray("is symlink: "), fgYellow(cur_symlink_path)
         cur_symlink_path = expandSymlink(cur_symlink_path)
-      cur_symlink_path = expandFilename(cur_symlink_path)
+        info "symlink expanded: ", fgRed(cur_symlink_path)
+      if cur_symlink_path[0] != '/':
+        info fgMagenta("Warning: Symlink '" & dest_file &
+                      "' is/points to a relative symlink.")
+      try:
+        cur_symlink_path = expandFilename(cur_symlink_path)
+      except: discard  # file doesn't exists at the symlink, symlink was broken
       let new_symlink_path = expandFilename(source_file)
       if cur_symlink_path == new_symlink_path:
-        echo "already good: ", f.path
+        info fgDarkGray("already good: "), f.path
         continue
 
     if existsFile(dest_file) or existsDir(dest_file) or
@@ -94,8 +110,10 @@ proc create_links(source, destination: string, skip = ignore_list) =
 
     if symlinkExists(source_file):
       source_file = expandSymlink(source_file)
-    echo "- creating symlink from ", source_file, " to ", dest_file
+    info "- creating symlink from ", source_file,
+      "\n                      to ", dest_file
     createSymlink(source_file, dest_file)
+  indent -= 1
 
 if not existsDir(dotfiles / ".config"):
   echo "The dotfiles folder doesn't contain a .config folder"
