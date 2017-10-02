1a5d7f9afa96e4d09043fd10db8c92fa8a34dc98
Erwan Ameil <wan@idlewan.com>
2017-10-02 11:59:47 +0100

    KDE font size, nvim-nim update, time function for fish and fix a fish var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,12 @@ bind \co "rcd ; commandline -f repaint"
 set -gx EDITOR nvim
 set -gx ANDROID_HOME /opt/android-sdk/
 set -U fish_greeting
-set -U -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+#set -U -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 set -gx BROWSER /usr/bin/firefox
 
 source ~/.config/fish/other.fish
 
 if status --is-interactive
     cd ~/FOLDERS
+    cat ~/.alerts
 end
---
+++ .config/fish/functions/time.fish
@@ -0,0 +1,4 @@
+function time --description="Measure the execution time of a program"
+    command time --portability $argv
+end
+
